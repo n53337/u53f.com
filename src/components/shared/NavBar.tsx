8f586ced6a60e6e76cf55316ea402363623c360e
n53f <n53337@users.noreply.github.com>
2023-02-15 11:37:50 +0000

    fix navbar layer index

--- src/components/shared/NavBar.tsx
+++ src/components/shared/NavBar.tsx
@@ -50,7 +50,7 @@ const NavBar = () => {
         <nav
           className={`${
             !isTriggered && "hidden"
-          } absolute left-0 right-0 mt-2 py-4 bg-white flex flex-col items-center gap-6 transition-all`}
+          } absolute left-0 right-0 mt-2 py-4 bg-white flex flex-col items-center gap-6 transition-all z-50 shadow-xl`}
         >
           <Link to="#">
             <Text variant="link">home</Text>
