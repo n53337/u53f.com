29712b63c3d6e7ec57a90f23dd89483209b9be05
n53f <n53337@users.noreply.github.com>
2023-02-26 14:56:03 +0000

    MDX component redesign

--- src/utils/MDXComponent.tsx
+++ src/utils/MDXComponent.tsx
@@ -1,11 +1,71 @@
-import Text from "@/ui/Text";
 import { MDXComponents } from "mdx/types";
+import Text from "@/ui/Text";
+import { Link } from "react-router-dom";
+import { CommandLineIcon } from "@heroicons/react/24/solid";
 
 const MDXComponent: MDXComponents = {
   h1: (props) => <Text variant="header" children={props.children} />,
   h2: (props) => <Text variant="title" children={props.children} />,
   p: (props) => (
     <Text variant="text" children={props.children} className="opacity-100" />
+  ),
+  a: (props) => (
+    <Link to={props.href || "#"}>
+      <Text
+        variant="link"
+        children={props.children}
+        className="inline underline text-purple-700"
+      />
+    </Link>
+  ),
+  blockquote: (props) => (
+    <blockquote
+      className="pl-2 italic border-l-4 border-l-gray-500 rounded-sm opacity-75"
+      children={props.children}
+    />
+  ),
+  ol: (props) => (
+    <ol className="list-decimal">
+      <Text variant="text" children={props.children} className="opacity-100" />
+    </ol>
+  ),
+  ul: (props) => (
+    <ul className="list-disc">
+      <Text variant="text" children={props.children} className="opacity-100" />
+    </ul>
+  ),
+  code: (props) => (
+    <Text
+      variant="shell"
+      children={props.children}
+      className="inline bg-gray-300 px-1"
+    />
+  ),
+  pre: (props) => (
+    <div className="w-full flex flex-col max-w-2xl shadow-md">
+      <div className="bg-black text-white px-4 py-2 flex justify-between items-center">
+        <CommandLineIcon className="w-6" />
+        <Text
+          variant="shell"
+          children={
+            props.children.props.className &&
+            props.children.props.className.slice(9)
+          }
+          className="text-white"
+        />
+      </div>
+      <div
+        // onClick={() => console.log(props.children.props.className.slice(9))}
+        className="max-h-48 md:max-h-96 flex flex-col gap-3 overflow-auto bg-white text-black p-4 border border-black"
+      >
+        {props.children.props.children &&
+          props.children.props.children
+            .split("\n")
+            .map((e: string, index: number) => (
+              <Text key={Date.now() + index} variant="shell" children={e} />
+            ))}
+      </div>
+    </div>
   ),
 };
 
