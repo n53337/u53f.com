be2f71dde71e8cfc546740af4ee5a6ac1b501886
u53f <scalewithusef@gmail.com>
2023-11-09 11:48:00 +0100

    test: testing CI/CD

--- src/components/home/Hero.tsx
+++ src/components/home/Hero.tsx
@@ -6,7 +6,7 @@ const Hero = () => {
   return (
     <section className="py-20 flex flex-col justify-center gap-10 md:gap-20 place-items-center relative">
       <Text variant="header" className="text-center lg:px-16 xl:px-24">
-        Hi there, I'm Youssef Houffaine and I specialize in crafting beautifully designed websites that deliver outstanding user experiences.
+        Hi TEST there, I'm Youssef Houffaine and I specialize in crafting beautifully designed websites that deliver outstanding user experiences.
       </Text>
       <Shell />
       <a href="#about">
